66588f3dc7f06cb8bba84ac4221100f8c080b341
wook <edisen315@gmail.com>
2024-01-09 15:47:51 +0900

    insert 완료

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('15.164.215.62:27017', username='dbadmin', password='admin1234')
+db = client.test
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("index.html", title = 'hello jinja')
+    db.users.insert_one({'name':'name1', 'age':21})
+    return render_template("index.html", title='hello jinja')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8080)
