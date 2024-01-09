2bb33553b537ae940253cdd10c4e311d955c7ede
SungJunMoon <ansdj3523@gmail.com>
2024-01-09 21:54:49 +0900

    make session login

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from session import app_session
 
 
-
 app = Flask(__name__)
+app.secret_key = 'moon'
 client = MongoClient('15.164.215.62:27017', username='dbadmin', password='admin1234')
 db = client.test
 
@@ -13,40 +13,40 @@ app.register_blueprint(app_session) # blueprint 등록
 
 @app.route('/')
 def hello_world():  # put application's code here
-    db.users.insert_one({'name':'name1', 'age':21})
-    return render_template("index.html", title='hello jinja')
+    return render_template("index.html")
 
 # 로그인
 @app.route('/login', methods=['GET'])
 def login():
     return render_template('login.html')
 
-# 로그인
+# 로그인 성공 시
 @app.route('/loginOk', methods=['POST'])
 def loginOk():
-    error = None
     username = request.form['username']
     password = request.form['password']
+
     check = db.users.find_one({'name' : username, 'password': password})
 
-    print('메소드 들어옴1')
-
     if check:
-        print('메소드 들어옴2')
-        return render_template('index.html', name=username)
+        session['name'] = username
+        if 'gender' in check:
+            session['gender'] = check['gender']
+            # session['_id'] = check['_id']
+            
+        return redirect(url_for('hello_world'))
     else:
-        error = '로그인 실패'
-        print('메소드 들어옴3')
         return render_template('login.html')
 
+# 회원가입
 @app.route('/join', methods=['POST'])
 def join():
     username = request.form['username']
     gender = request.form['gender']
     password = request.form['password']
-    print(gender)
+    passwordcheck = request.form['passwordcheck']
 
-    if username != None and gender != None and password != None:
+    if username != None and gender != None and password != None and password == passwordcheck :
         db.users.insert_one({'name': username, 'password': password, 'gender': gender})
         print('회원가입 성공')
         return render_template('login.html')
