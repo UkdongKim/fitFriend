520951e204699bc60dde0af9e557a54d78221183
jaenam <jaenam2060@gmail.com>
2024-01-10 16:35:19 +0900

    modified:   __pycache__/session.cpython-312.pyc modified:   session.py modified:   static/css/stylesheet.css modified:   templates/guide.html modified:   templates/index.html modified:   templates/login.html

--- session.py
+++ session.py
@@ -14,8 +14,9 @@ app_session = Blueprint('app_session', __name__)
 @app_session.route('/session', methods = ['POST'])
 def make_session():
 
-    userName = request.form['userName']
-    userId = request.form['userId']
+    #해결때까지 주석처리
+    # userName = request.form['userName']
+    # userId = request.form['userId']
     day = request.form['day']
     time = request.form['time']
     field = request.form['field']
@@ -23,11 +24,13 @@ def make_session():
     show = True
 
     session = {
-        'userName':userName,
-        'userId':userId,
+        #위와 동
+        # 'userName':userName,
+        # 'userId':userId,
         'day':day,
         'time':time,
         'field':field,
+        'current_member': 1, #추가했습니다 (사람이 참가를 선택하면 1 오르도록)
         'max_member':max_member,
         'show':show
     }
@@ -35,11 +38,18 @@ def make_session():
     result = db.session.insert_one(session)
 
     if result.inserted_id:
-        return jsonify({'success' : True, 'message' : 'Data inserted success'}), 201
+#        return jsonify({'success': True, 'message' : 'Data inserted success'}), 201, 아래의 수정본으로 적용 시 성공 정상적으로 뜸 
+        return jsonify({'result':'success', 'message' : 'Data inserted success'}), 201
     else:
         return jsonify({'success': False, 'message': 'Failed to insert data'}), 500
 
+
 #세션 조회
+# @app_session.route('/tile', methods = ['GET'])
+# def show_session():
+
+#     tiles = list(db.)
+
 
 
 
